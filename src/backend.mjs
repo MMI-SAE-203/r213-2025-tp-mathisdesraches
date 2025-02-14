044e525a2143730fcb2c159814d5a71c7be5b8f3
mathisdesraches <mathis.desraches@edu.univ-fcomte.fr>
2025-02-14 10:58:19 +0100

    add offre

--- src/backend.mjs
+++ src/backend.mjs
@@ -1,9 +1,11 @@
+
+
 import PocketBase from "pocketbase";
 const pb = new PocketBase("http://127.0.0.1:8090");
 
 export async function getOffres() {
     try {
-        let data = await pb.collection('Maison').getFullList({
+        let data = await pb.collection('maison').getFullList({
             sort: '-prix',
         });
         data = data.map((offre) => {
@@ -25,5 +27,21 @@ export async function getOffre(id) {
     } catch (error) {
         console.log('Une erreur est survenue en lisant la maison', error);
         return null;
+    }
+}
+
+export async function addOffre(house) {
+    try {
+        await pb.collection('maison').create(house);
+        return {
+            success: true,
+            message: 'Offre ajoutée avec succès'
+        };
+    } catch (error) {
+        console.log('Une erreur est survenue en ajoutant la maison', error);
+        return {
+            success: false,
+            message: 'Une erreur est survenue en ajoutant la maison'
+        };
     }
 }
